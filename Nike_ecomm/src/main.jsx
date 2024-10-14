9549818e2b8e4377cd818c86273566da9af48714
Imasuengarret <garretharry@gmail.com>
2024-10-14 16:37:54 +0100

    made a case change in app.js

--- Nike_ecomm/src/main.jsx
+++ Nike_ecomm/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import Store from './app/store.js'
+import Store from './app/Store.js'
 import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
